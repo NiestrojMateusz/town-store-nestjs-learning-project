671a6d68033399e206e41c47e0cdca17c5dea96d
m.niestroj <mateusz.niestroj@equiqo.com>
2023-11-23 07:36:57 +0100

    Zmiany dla lekcji 3

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,47 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 
-@Controller()
+interface Category {
+  id: number;
+  name: string;
+}
+
+@Controller('categories')
 export class AppController {
+  private categories: Category[] = [
+    { id: 1, name: 'Groceries' },
+    { id: 2, name: 'Cosmetics' },
+    { id: 3, name: 'Toys' },
+    { id: 4, name: 'Dairy' },
+    { id: 5, name: 'Fashion' },
+    { id: 6, name: 'Electronics' },
+    { id: 7, name: 'Games' },
+  ];
+
+  private nextId = 8;
+
   @Get()
-  getHello(): string {
-    return 'Hello world! ';
+  getAll(): Category[] {
+    return this.categories;
+  }
+
+  @Get(':id')
+  getSingleCategory(@Param('id') categoryId: string) {
+    return this.categories.find((c) => c.id === Number(categoryId));
+  }
+
+  @Post()
+  addNewCategory(@Body() payload: { name: string }) {
+    const category: Category = { id: this.nextId++, ...payload };
+    this.categories.push(category);
+    return category;
+  }
+
+  @Delete(':id')
+  delateCategory(@Param('id') categoryId: string) {
+    this.categories = this.categories.filter(
+      (cat) => cat.id !== Number(categoryId),
+    );
+
+    return categoryId;
   }
 }
