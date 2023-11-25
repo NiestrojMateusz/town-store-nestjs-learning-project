fd019e762762be4af4840d5619945ddafefb3ef5
m.niestroj <mateusz.niestroj@equiqo.com>
2023-11-25 14:01:28 +0100

    Controller basics

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,15 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpStatus,
+  NotFoundException,
+  Param,
+  ParseIntPipe,
+  Post,
+} from '@nestjs/common';
+import { NewCategoryDto } from './new-category-dto';
 
 interface Category {
   id: number;
@@ -19,29 +30,51 @@ export class AppController {
 
   private nextId = 8;
 
+  private getCategoryById(categoryId: number) {
+    const category = this.categories.find((c) => c.id === categoryId);
+
+    return category;
+  }
+
   @Get()
   getAll(): Category[] {
     return this.categories;
   }
 
   @Get(':id')
-  getSingleCategory(@Param('id') categoryId: string) {
-    return this.categories.find((c) => c.id === Number(categoryId));
+  getSingleCategory(
+    @Param(
+      'id',
+      new ParseIntPipe({ errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE }),
+    )
+    categoryId: number,
+  ) {
+    const category = this.getCategoryById(categoryId);
+
+    if (!category) {
+      throw new NotFoundException(`category with id: ${categoryId} not found`);
+    }
+
+    return category;
   }
 
   @Post()
-  addNewCategory(@Body() payload: { name: string }) {
-    const category: Category = { id: this.nextId++, ...payload };
+  addNewCategory(@Body() payload: NewCategoryDto) {
+    const category: Category = { ...payload, id: this.nextId++ };
     this.categories.push(category);
     return category;
   }
 
   @Delete(':id')
-  delateCategory(@Param('id') categoryId: string) {
-    this.categories = this.categories.filter(
-      (cat) => cat.id !== Number(categoryId),
-    );
+  delateCategory(@Param('id') categoryId: number) {
+    const category = this.getCategoryById(categoryId);
 
-    return categoryId;
+    if (!category) {
+      throw new NotFoundException(`category with id: ${categoryId} not found`);
+    }
+
+    this.categories = this.categories.filter((cat) => cat.id !== category.id);
+
+    return { id: categoryId, removed: true };
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn', 'log'],
   });
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   await app.listen(3000);
   console.log('Server listen on http://localhost:3000');
 }
